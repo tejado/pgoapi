fec0672dcd353e89e51dd8646a56d1fb2a9ecced
Chlodochar <Chlodochar@users.noreply.github.com>
2016-07-26 18:58:14 +0200

    changes

--- .travis/install_protobuf.sh
+++ .travis/install_protobuf.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+set -o errexit
+
 cd $SRCDIR
-wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
-tar -xzvf protobuf-2.4.1.tar.gz
-pushd protobuf-2.4.1 && ./configure --prefix=$PREFIX && make && make install && popd
+PROTOBUF=protobuf-2.4.1
+ARCHIVE=$PROTOBUF.tar.gz
+
+if [ -d $DIR ]
+then
+    echo "Protobuf already cached. Skipping compilation and installation."
+    exit 0
+fi
+
+wget https://protobuf.googlecode.com/files/$ARCHIVE
+tar -xzvf $ARCHIVE
+pushd $PROTOBUF
+./configure --prefix=$PREFIX
+make
+make install
+popd
