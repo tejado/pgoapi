6d6363171fce43cbc9fe68353a727217ca6895df
tegob <tegob@users.noreply.github.com>
2016-08-08 17:51:57 +0200

    Remove `NoPlayerPositionSetException` import

--- pgoapi/pgoapi.py
+++ pgoapi/pgoapi.py
@@ -34,7 +34,7 @@ from . import __title__, __version__, __copyright__
 from pgoapi.rpc_api import RpcApi
 from pgoapi.auth_ptc import AuthPtc
 from pgoapi.auth_google import AuthGoogle
-from pgoapi.exceptions import AuthException, NotLoggedInException, ServerBusyOrOfflineException, NoPlayerPositionSetException, EmptySubrequestChainException
+from pgoapi.exceptions import AuthException, NotLoggedInException, ServerBusyOrOfflineException, EmptySubrequestChainException
 
 from . import protos
 from POGOProtos.Networking.Requests_pb2 import RequestType
