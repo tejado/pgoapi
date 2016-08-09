915cfa899d22aa1b9ea176abb265c357e2445c6e
OurFlagIsMined <OurFlagIsMined@users.noreply.github.com>
2016-08-09 05:46:04 -0400

    Add auth service as an optional parameter

--- scripts/accept-tos.py
+++ scripts/accept-tos.py
@@ -11,10 +11,10 @@ import pprint
 import time
 import threading
 
-def accept_tos(username, password):
+def accept_tos(username, password, auth='ptc'):
 	api = PGoApi()
 	api.set_position(40.7127837, -74.005941, 0.0)
-	api.login('ptc', username, password)
+	api.login(auth, username, password)
 	time.sleep(2)
 	req = api.create_request()
 	req.mark_tutorial_complete(tutorials_completed = 0, send_marketing_emails = False, send_push_notifications = False)
@@ -22,4 +22,8 @@ def accept_tos(username, password):
 	print('Accepted Terms of Service for {}'.format(username))
 	#print('Response dictionary: \r\n{}'.format(pprint.PrettyPrinter(indent=4).pformat(response)))
 
+"""auth service defaults to ptc if not given"""
+
 accept_tos('username', 'password')
+accept_tos('username2', 'password', 'ptc')
+accept_tos('username3', 'password', 'google')
